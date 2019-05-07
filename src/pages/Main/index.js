a853c6fe80c650e13b8081e5f4390f25bcedd496
Leandro Moro <33632804+moroleandro@users.noreply.github.com>
2019-05-07 01:01:26 -0300

    Aplicação boxCloud

---
+++ src/pages/Main/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import  './styles.css';
+import logo from '../../assets/logo.png'
+
+
+export default class Main extends Component {
+  state = {
+    newBox: '',
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const response = await api.post('boxes', {
+      title: this.state.newBox
+    });
+
+    this.props.history.push(`/box/${response.data._id}`);
+  };
+
+  handleInputChange = (e) =>{
+     this.setState({ newBox: e.target.value });
+  }
+
+  render() {
+    return (
+        <div id="main-container">
+        <form onSubmit={this.handleSubmit}>
+          <img src={logo} alt="" id="logo-main"/>
+          <input value={this.state.newBox} onChange={this.handleInputChange} placeholder="Nome da box (pasta)" />
+          <button type="submit">C R I A R</button>
+        </form>
+        </div>
+  
+    );
+}
+}
